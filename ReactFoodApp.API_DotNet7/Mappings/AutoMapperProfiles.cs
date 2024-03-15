be20fbad209c42c27c65a02daa731fc032c6e73a
Samuel Yin <wsgddjy@live.com>
2024-03-15 13:29:11 +1000

    add AutoMapper

---
+++ ReactFoodApp.API_DotNet7/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReactFoodApp.API_DotNet7.Models.Domain;
+using ReactFoodApp.API_DotNet7.Models.Dtos;
+
+namespace ReactFoodApp.API_DotNet7.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Meal, MealDto>().ReverseMap();
+        }
+    }
+}
--- ReactFoodApp.API_DotNet7/Program.cs
+++ ReactFoodApp.API_DotNet7/Program.cs
@@ -1,25 +1,29 @@
-var builder = WebApplication.CreateBuilder( args );
+using ReactFoodApp.API_DotNet7.Mappings;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers( );
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer( );
-builder.Services.AddSwaggerGen( );
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//add automapper
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
-var app = builder.Build( );
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment( ))
+if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger( );
-    app.UseSwaggerUI( );
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection( );
+app.UseHttpsRedirection();
 
-app.UseAuthorization( );
+app.UseAuthorization();
 
-app.MapControllers( );
+app.MapControllers();
 
-app.Run( );
+app.Run();
