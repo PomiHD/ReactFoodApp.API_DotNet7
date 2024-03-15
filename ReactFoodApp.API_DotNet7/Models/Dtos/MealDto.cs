c8ee84e7bf32d5ddcbf18b14e0d5d6fcaaf9049b
Samuel Yin <wsgddjy@live.com>
2024-03-15 12:15:32 +1000

    add meal Dto

---
+++ ReactFoodApp.API_DotNet7/Models/Dtos/MealDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ReactFoodApp.API_DotNet7.Models.Dtos
+{
+    public class MealDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
