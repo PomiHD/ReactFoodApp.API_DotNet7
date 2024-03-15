3cf788ac8d3df788cf97480bb8a3dedf5ed89689
Samuel Yin <wsgddjy@live.com>
2024-03-15 12:13:37 +1000

    add Meal domain model

---
+++ ReactFoodApp.API_DotNet7/Models/Domain/Meal.cs
@@ -0,0 +1,11 @@
+﻿namespace ReactFoodApp.API_DotNet7.Models.Domain
+{
+    public class Meal
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
