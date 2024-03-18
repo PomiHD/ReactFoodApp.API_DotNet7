ae10ebd11dbac0fe51fe5d8227039e5ac7f9530a
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-18 14:59:00 +1000

    add async method to get all meals.

---
+++ ReactFoodApp.API_DotNet7/Controllers/MealController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactFoodApp.API_DotNet7.Data;
+
+namespace ReactFoodApp.API_DotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealController : ControllerBase
+    {
+        private readonly FoodAppDbContext _foodAppDbContext;
+
+        public MealController(FoodAppDbContext foodAppDbContext)
+        {
+            _foodAppDbContext = foodAppDbContext;
+        }
+
+        // get all meals
+        //Get: api/Meals
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var mealsDomainModel = await _foodAppDbContext.Meals.ToListAsync();
+
+            return Ok(mealsDomainModel);
+        }
+    }
+}
