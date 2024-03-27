8c8ce592f9fba9fe19899d121ed9aff3d422d652
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-28 02:03:10 +1000

    Add order controller.

--- ReactFoodApp.API_DotNet7/Controllers/OrdersController.cs
+++ ReactFoodApp.API_DotNet7/Controllers/OrdersController.cs
@@ -1,5 +1,9 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using ReactFoodApp.API_DotNet7.Models.Domain;
+using ReactFoodApp.API_DotNet7.Models.Dtos;
+using ReactFoodApp.API_DotNet7.Models.Repositories;
 
 namespace ReactFoodApp.API_DotNet7.Controllers
 {
@@ -8,8 +12,27 @@ namespace ReactFoodApp.API_DotNet7.Controllers
     [EnableCors("CustomCorsPolicy")]
     public class OrdersController : ControllerBase
     {
-        // place order
-        // Post: api/Orders
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
 
+        public OrdersController(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderDto orderDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var order = _mapper.Map<Order>(orderDto);
+            var createdOrder = await _orderRepository.CreateOrderAsync(order);
+
+            return CreatedAtAction(nameof(CreateOrder), new { id = createdOrder.Id }, createdOrder);
+        }
     }
 }
